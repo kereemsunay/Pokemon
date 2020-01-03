2a427a4da8b7daa935d34776d4396ff116764962
Kerem Sunay <sunay.kerem@outlook.com>
2020-01-03 22:14:46 +0300

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Kerem on 3.01.2020.
+//  Copyright © 2020 Kerem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
